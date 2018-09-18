ada8a87aab6a7cc80b7cb2309b3ac5a2e3ba0322
Tomasz Cieślar <cieslar.tomek@gmail.com>
2018-09-18 11:47:30 +0200

    developer_verification statistics tests

--- app/lib/developer_verification_checklist.rb
+++ app/lib/developer_verification_checklist.rb
@@ -1,5 +1,6 @@
 class DeveloperVerificationChecklist
-  def initialize(markdown_checklist_statistics)
+  def initialize(markdown_checklist_statistics, level)
     @markdown_checklist_statistics = markdown_checklist_statistics
+    @level = level
   end
 end
